51d9b00a6f6adb47577b31485463331f617964e9
Steven Tupper <10thElement@gmail.com>
2017-10-18 23:00:33 +0200

    Added comments for permutation enumerator

--- AlgorithmTrickery/PermutationEnumerator.cs
+++ AlgorithmTrickery/PermutationEnumerator.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 namespace AlgorithmTrickery
 {
+    /// <summary>
+    /// A class that provides enumeration through all permutations of an array of items.
+    /// Only the current permutation is stored at any time, reducing the memory needed for 
+    /// working with permutations.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class PermutationEnumerator<T> : IEnumerator<T[]>,IEnumerable<T[]> where T : IComparable<T>
 
     {
@@ -14,6 +20,11 @@ namespace AlgorithmTrickery
         double numPermutations = -1;
         double currentPermutationNumber = -1;
 
+        /// <summary>
+        /// The constructor takes the array of items to find the permutations for
+        /// and creates a shallow copy of the array. This copied array is sorted
+        /// </summary>
+        /// <param name="items"></param>
         public PermutationEnumerator(T [] items){
             currentPermutation = new T[items.Length];
             items.CopyTo(currentPermutation, 0);
@@ -21,6 +32,9 @@ namespace AlgorithmTrickery
             numPermutations = Factorial.GetFactorial(items.Length);
         }
         
+        /// <summary>
+        /// Get current permutation
+        /// </summary>
         object IEnumerator.Current
         {
             get
@@ -29,6 +43,9 @@ namespace AlgorithmTrickery
             }
         }
 
+        /// <summary>
+        /// Get current permutation
+        /// </summary>
         T[] IEnumerator<T[]>.Current
         {
             get
@@ -42,6 +59,10 @@ namespace AlgorithmTrickery
             
         }
 
+        /// <summary>
+        /// Get next permutation. Returns whether there are more more permutations
+        /// </summary>
+        /// <returns></returns>
         bool IEnumerator.MoveNext()
         {
             if (currentPermutationNumber >= 0)
@@ -52,10 +73,13 @@ namespace AlgorithmTrickery
             return currentPermutationNumber < numPermutations;
         }
 
+        /// <summary>
+        /// Resets permuation enumerator to first enumeration
+        /// </summary>
         void IEnumerator.Reset()
         {
             Array.Sort(currentPermutation);
-            currentPermutationNumber = 0;
+            currentPermutationNumber = -1;
         }
 
         IEnumerator<T[]> IEnumerable<T[]>.GetEnumerator()
