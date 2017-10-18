3fe41ea3ff0746118926922fe003336d9805ae29
Steven Tupper <10thElement@gmail.com>
2017-10-18 22:56:39 +0200

    Created permutation enumerator and added unit test for it.

---
+++ AlgorithmTrickery/PermutationEnumerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTrickery
+{
+    public class PermutationEnumerator<T> : IEnumerator<T[]>,IEnumerable<T[]> where T : IComparable<T>
+
+    {
+        T[] currentPermutation = null;
+        double numPermutations = -1;
+        double currentPermutationNumber = -1;
+
+        public PermutationEnumerator(T [] items){
+            currentPermutation = new T[items.Length];
+            items.CopyTo(currentPermutation, 0);
+            Array.Sort(currentPermutation);
+            numPermutations = Factorial.GetFactorial(items.Length);
+        }
+        
+        object IEnumerator.Current
+        {
+            get
+            {
+                return currentPermutation;
+            }
+        }
+
+        T[] IEnumerator<T[]>.Current
+        {
+            get
+            {
+                return currentPermutation;
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (currentPermutationNumber >= 0)
+            {
+                Permutations.NextPermutation(currentPermutation);
+            }
+            ++currentPermutationNumber;
+            return currentPermutationNumber < numPermutations;
+        }
+
+        void IEnumerator.Reset()
+        {
+            Array.Sort(currentPermutation);
+            currentPermutationNumber = 0;
+        }
+
+        IEnumerator<T[]> IEnumerable<T[]>.GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator<T[]>)this;
+        }
+    }
+}
---
+++ AlgorithmTrickeryTests/PermutationEnumeratorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmTrickery;
+
+namespace AlgorithmTrickeryTests
+{
+    [TestClass]
+    public class PermutationEnumeratorTests
+    {
+        [TestMethod]
+        public void TestPermutationEnumerator()
+        {
+            int[] testArrayLengths = new int[] { 1, 3, 4, 7 };
+            int[] seeds = new int[] { 1, 54353, 6363, 756757 };
+            /*
+                Instantiate and seed random number generator within
+                method to keep array elements consistent between tests
+            */
+            Random arrayNumberGenerator = new Random(2017);
+
+            foreach (int arrayLength in testArrayLengths)
+            {
+                //foreach (int seed in seeds)
+                //{
+
+                //generate random array of given lenth
+                int[] array = new int[arrayLength];
+                for (int i = 0; i < arrayLength; ++i)
+                {
+                    array[i] = i + 1;
+                }
+
+                int[][] permutations = Permutations.GetPermutations<int>(array);
+
+                PermutationEnumerator<int> enumerator = new PermutationEnumerator<int>(array);
+
+                int currentPermutation = 0;
+
+                foreach (int [] currentEnumeratorPermutation in enumerator)
+                {
+                    Assert.AreEqual(currentEnumeratorPermutation.Length, permutations[currentPermutation].Length);
+                    
+                    for (int j = 0; j< currentEnumeratorPermutation.Length; ++j)
+                    {
+                        Assert.AreEqual(currentEnumeratorPermutation[j], permutations[currentPermutation][j]);
+                    }
+
+                    ++currentPermutation;
+                }
+
+            }
+        }
+    }
+}
